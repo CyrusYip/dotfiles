dfc79185916363c1a0eb431ef6e53b9af56ea5f2
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2022-05-30 11:30:58 +0800

    fish,zsh: source thefuck when it exists

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
 end
+
+if [ -x "$(command -v thefuck)" ]
+    thefuck --alias | source
+end
